9e870352ad11bc9674fec8088e3ada937c43a678
BeastyFR <thisIsNotA@RealEmail.com>
2022-08-18 05:51:49 +0200

    feat(Authentication): Ability to connect to twitch server

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,55 @@
-class Bot
+import { BotOptions } from "./botoption";
+var tmi = require('tmi.js');
+
+export class Bot
 {
+	options: BotOptions;
+	client: any
+
+	constructor()
+	{
+		this.options = new BotOptions('dist/authSettings.json');
+		this.client = new tmi.client(this.options.getIdentityJson());
+
+		// Register our event handlers (defined below)
+		this.client.on('message', this.onMessageHandler);
+		this.client.on('connected', this.onConnectedHandler);
+		this.client.connect();
+
+	}
+
+
+	// Called every time a message comes in
+	onMessageHandler(target: string, context: any, msg: string, self: any)
+	{
+		if (self) { return; } // Ignore messages from the bot
+
+		// Remove whitespace from chat message
+		const commandName = msg.trim();
+
+		// If the command is known, let's execute it
+		if (commandName === '!dice')
+		{
+			const num = this.rollDice();
+			this.client.say(target, `You rolled a ${num}`);
+			console.log(`* Executed ${commandName} command`);
+		} else
+		{
+			console.log(`* Unknown command ${commandName}`);
+		}
+	}
+
+	// Function called when the "dice" command is issued
+	rollDice()
+	{
+		const sides = 6;
+		return Math.floor(Math.random() * sides) + 1;
+	}
+
+	// Called every time the bot connects to Twitch chat
+	onConnectedHandler(addr: string, port: number)
+	{
+		console.log(`* Connected to ${addr}:${port}`);
+	}
 
 }
